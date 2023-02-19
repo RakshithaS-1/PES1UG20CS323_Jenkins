e0e553395a583cb6f091fdc27d64c10a4ab73d34
RakshithaS-1 <116474864+RakshithaS-1@users.noreply.github.com>
2023-02-19 21:04:35 +0530

    update hello.cpp

--- main/1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(void) {
-  cout << "Hello, this is PES1UG20CS323\n";  
-  cout << "This is from cpp file used for Jenkins Pipeline\n";
-  return 0;
-}
