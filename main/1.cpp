9643c55a644dd6c77217e4aa1ac693388220d0c9
RakshithaS-1 <116474864+RakshithaS-1@users.noreply.github.com>
2023-02-19 20:54:52 +0530

    create 1.cpp

---
+++ main/1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, this is PES1UG20CS323\n";  
+  cout << "This is from cpp file used for Jenkins Pipeline\n";
+  return 0;
+}
